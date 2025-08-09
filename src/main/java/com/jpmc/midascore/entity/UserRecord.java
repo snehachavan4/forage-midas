84b1fd1a59664ec077ca11167ca84f651bede159
snehachavan4 <sneha.42006@gmail.com>
2025-08-09 18:04:36 +0530

    Update UserRecord.java

--- src/main/java/com/jpmc/midascore/entity/UserRecord.java
+++ src/main/java/com/jpmc/midascore/entity/UserRecord.java
@@ -1,6 +1,30 @@
 package com.jpmc.midascore.entity;
 
 import jakarta.persistence.*;
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class TransactionRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id", nullable = false)
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "recipient_id", nullable = false)
+    private User recipient;
+
+    private BigDecimal amount;
+
+    private String transactionId;
+
+    // getters and setters
+}
 
 @Entity
 public class UserRecord {
