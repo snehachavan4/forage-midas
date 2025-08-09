1da3c98443be3c3dd554576a6a4837d16b25e5a6
snehachavan4 <sneha.42006@gmail.com>
2025-08-09 18:06:52 +0530

    Update UserRepository.java

--- src/main/java/com/jpmc/midascore/repository/UserRepository.java
+++ src/main/java/com/jpmc/midascore/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.jpmc.midascore.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransactionRecordRepository extends JpaRepository<TransactionRecord, Long> {
+}
 
 import com.jpmc.midascore.entity.UserRecord;
 import org.springframework.data.repository.CrudRepository;
